537963fee56577da5afe921e326e68f0f0babc07
KingMus <marcofm47@gmail.com>
2018-05-09 20:45:55 +0200

    added color regocnition blue

--- code/opencv-module.py
+++ code/opencv-module.py
@@ -6,9 +6,14 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Display the resulting frame
-    cv.imshow('frame',gray)
+    cv.imshow('frame',mask)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
