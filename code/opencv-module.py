c94305d41ec5ea0fa2cf6f656884e1faf8b6acf6
KingMus <marcofm47@gmail.com>
2018-05-10 22:02:48 +0200

    added tracking of object and drawing of trace

--- code/opencv-module.py
+++ code/opencv-module.py
@@ -1,19 +1,90 @@
+from collections import deque
 import numpy as np
 import cv2 as cv
+import argparse
+import imutils
 
-cap = cv.VideoCapture(0)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+
+cap = cv.VideoCapture(1)
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+	# resize the frame and convert it to the HSV
+	# color space
+    frame = imutils.resize(frame, width=600)
+
     # Our operations on the frame come here
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-        # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    # define range of blue color in HSV,  then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    #lower_blue = np.array([110,78,50])
+    #upper_blue = np.array([130,255,255])
+    mask = cv.inRange(hsv, greenLower, greenUpper)
+    mask = cv.erode(mask, None, iterations=2)
+    mask = cv.dilate(mask, None, iterations=2)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+    cnts = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    # only proceed if at least one contour was found
+
+    if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+        c = max(cnts, key=cv.contourArea)
+        ((x, y), radius) = cv.minEnclosingCircle(c)
+        M = cv.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+        if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+            cv.circle(frame, (int(x), int(y)), int(radius), (255,255, 255), 2)
+            cv.circle(frame, center, 5, (0, 0, 255), -1)
+
+
+    # update the points queue
+    pts.appendleft(center)
+
+	# loop over the set of tracked points
+    for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    #res = cv.bitwise_and(frame,frame, mask= mask)
     # Display the resulting frame
-    cv.imshow('frame',mask)
+    cv.imshow('frame',frame)
+
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
