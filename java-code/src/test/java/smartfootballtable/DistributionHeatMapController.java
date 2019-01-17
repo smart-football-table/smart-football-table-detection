759a9b855c36e8d637d980c47c5b2556ae1ed15d
KingMus <bomberurs+it@gmail.com>
2019-01-17 18:28:45 +0100

    added test

--- java-code/src/test/java/smartfootballtable/DistributionHeatMapController.java
+++ java-code/src/test/java/smartfootballtable/DistributionHeatMapController.java
@@ -5,18 +5,40 @@ import java.util.List;
 
 public class DistributionHeatMapController {
 
-	public void configureFieldGranularity(int i) {
-		// TODO Auto-generated method stub
+	private List<Integer> list = new ArrayList<Integer>();
+	
+	private List<Integer> positions = new ArrayList<Integer>();
+	
+	public void configureFieldGranularity(int fieldCount) {
+		for (int i = 0; i < fieldCount; i++) {
+			list.add(0);
+		}
+		
 		
 	}
 
 	public List<Integer> getFields() {
-		
-		List<Integer> list = new ArrayList<Integer>();
-		
-		list.add(0);
-		
 		return list;
+	}
+
+	public void addPositionToField(int fieldNumber) {
+		positions.add(fieldNumber);
+		
+	}
+
+	public double getDistributionInPercentOfField(int fieldNumber) {
+		
+		double sum = 0;
+		
+		for (Integer fieldPosition : positions) {
+			
+			if(fieldNumber == fieldPosition) {
+				sum++;
+			}
+			
+		}
+		
+		return (sum/positions.size()) * 100;
 	}
 
 }
