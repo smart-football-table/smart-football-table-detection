199933d20d5d32a5725ce563edbdd8bc9e41c0d9
KingMus <bomberurs+it@gmail.com>
2019-01-17 19:47:55 +0100

    added horizontal support for heatmap

--- java-code/src/test/java/smartfootballtable/HeatMapTileCalculator.java
+++ java-code/src/test/java/smartfootballtable/HeatMapTileCalculator.java
@@ -2,8 +2,31 @@ package smartfootballtable;
 
 public class HeatMapTileCalculator {
 
-	public int calculateHeatMapId(int x, int y) {
-		return 1;
+	private int xHeatmapSize = 0;
+	private int yHeatMapSize = 0;
+
+	private int heatMapTilesPerColumn = 1;
+	private int heatMapTilesPerRow = 1;
+
+	public int calculateHeatMapId(double x, double y) {
+		
+		int sizePerHeatMapTile = xHeatmapSize / heatMapTilesPerColumn;
+
+		return (int) (Math.ceil(x / sizePerHeatMapTile));
+	}
+
+	public void setHeatMapSize(int xMaxOfGame, int yMaxOfGame) {
+		xHeatmapSize = xMaxOfGame;
+		yHeatMapSize = yMaxOfGame;
+	}
+
+	public void setheatMapTilesPerColumn(int heatMapTilesPerColumn) {
+		this.heatMapTilesPerColumn = heatMapTilesPerColumn;
+	}
+
+	public void setheatMapTilesPerRow(int heatMapTilesPerRow) {
+		this.heatMapTilesPerRow = heatMapTilesPerRow;
+
 	}
 
 }
