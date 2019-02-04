68194715b0bdee4dcef4fd4aef6d24bb4fa128e3
KingMus <bomberurs+it@gmail.com>
2019-02-04 23:57:32 +0100

    beautified test

--- java-code/src/test/java/de/fiduciagad/de/sft/test/GoalDetector.java
+++ java-code/src/test/java/de/fiduciagad/de/sft/test/GoalDetector.java
@@ -1,8 +1,11 @@
 package de.fiduciagad.de.sft.test;
 
+import fiduciagad.de.sft.main.BallPosition;
+
 public class GoalDetector {
 
-	public boolean isThereAGoal() {
+	public boolean isThereAGoal(BallPosition ballPosition_t, BallPosition ballPosition_tMinus1,
+			BallPosition ballPosition_tMinus2) {
 		return false;
 	}
 
--- java-code/src/test/java/de/fiduciagad/de/sft/test/GoalDetectorTest.java
+++ java-code/src/test/java/de/fiduciagad/de/sft/test/GoalDetectorTest.java
@@ -13,20 +13,59 @@ public class GoalDetectorTest {
 
 	private int xMaxOfGame = 1000;
 	private int yMaxOfGame = 500;
-	
-	
+
+	BallPosition ballPosition_t;
+	BallPosition ballPosition_tMinus1;
+	BallPosition ballPosition_tMinus2;
+
 	@Test
 	public void gettingThreePositionsMidfieldIsntAGoal() {
-		
-		BallPosition ballPosition_t = new BallPosition(300, 0, new Date(2));
-		BallPosition ballPosition_tMinus1 = new BallPosition(300, 0, new Date(1));
-		BallPosition ballPosition_tMinus2 = new BallPosition(300, 0, new Date(0));
-		
+
+		String gameField = // field with 20x5, each one is 10 pixel (200*50)
+				"----------------------," + //
+						"|00000000000000000000|," + //
+						"-00000000000000000000-," + //
+						" 00000001020300000000 ," + //
+						"-00000000000000000000-," + //
+						"|00000000000000000000|," + //
+						"----------------------";
+
+		initalizeBallPositionsFrom(gameField);
+
 		GoalDetector goalDetector = new GoalDetector();
-		
-		assertThat(goalDetector.isThereAGoal(), is(false));
-		
-		
+
+		assertThat(goalDetector.isThereAGoal(ballPosition_t, ballPosition_tMinus1, ballPosition_tMinus2), is(false));
+
+	}
+
+	private void initalizeBallPositionsFrom(String gameField) {
+
+		String[] linesFromField = gameField.split(",");
+
+		for (int i = 0; i < linesFromField.length; i++) {
+
+			String actualLine = linesFromField[i];
+
+			for (int j = 0; j < actualLine.toCharArray().length; j++) {
+
+				char character = actualLine.charAt(j);
+
+				int yCoordinate = (i - 1) * 10;
+				int xCoordinate = (j - 1) * 10;
+
+				if (character == '1') {
+					ballPosition_tMinus2 = new BallPosition(xCoordinate, yCoordinate, new Date());
+				}
+				if (character == '2') {
+					ballPosition_tMinus1 = new BallPosition(xCoordinate, yCoordinate, new Date());
+				}
+				if (character == '3') {
+					ballPosition_t = new BallPosition(xCoordinate, yCoordinate, new Date());
+				}
+
+			}
+		}
+
 	}
 
 }
