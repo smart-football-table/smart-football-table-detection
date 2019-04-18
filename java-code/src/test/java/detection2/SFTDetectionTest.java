d017fbe67fb5e1e6d6db6d4e6b93a6034359e441
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-04-18 19:07:33 +0200

    house keeping

--- java-code/src/test/java/detection2/SFTDetectionTest.java
+++ java-code/src/test/java/detection2/SFTDetectionTest.java
@@ -148,44 +148,6 @@ public class SFTDetectionTest {
 			this.y = y;
 		}
 
-		public static Position parse(String line) {
-			String[] values = line.split("\\,");
-			if (values.length == 3) {
-				Long timestamp = toLong(values[0]);
-				Double x = toDouble(values[1]);
-				Double y = toDouble(values[2]);
-				if (isValidTimestamp(timestamp) && isValidPosition(x, y)) {
-					return new Position(timestamp, x, y);
-				}
-			}
-			return null;
-		}
-
-		private static boolean isValidTimestamp(Long timestamp) {
-			return timestamp != null && timestamp >= 0;
-		}
-
-		private static boolean isValidPosition(Double x, Double y) {
-			// TODO test x/y > 1.0?
-			return x != null && y != null && x >= 0.0 && y >= 0.0;
-		}
-
-		private static Double toDouble(String val) {
-			try {
-				return Double.valueOf(val);
-			} catch (NumberFormatException e) {
-				return null;
-			}
-		}
-
-		private static Long toLong(String val) {
-			try {
-				return Long.valueOf(val);
-			} catch (NumberFormatException e) {
-				return null;
-			}
-		}
-
 	}
 
 	private static class Message {
@@ -326,23 +288,59 @@ public class SFTDetectionTest {
 	}
 
 	private void whenStdInInputWasProcessed() throws IOException {
-		Position prevRelPos = null;
 		Position prevAbsPos = null;
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
-				Position relPos = Position.parse(line);
+				Position relPos = parse(line);
 				if (relPos != null) {
 					Position absPos = table.toAbsolute(relPos);
 					sendPositions(relPos, absPos);
-					if (prevRelPos != null) {
+					if (prevAbsPos != null) {
 						sendMovement(new Movement(prevAbsPos, absPos));
 					}
-					prevRelPos = relPos;
 					prevAbsPos = absPos;
 				}
 			}
 
+		}
+	}
+
+	private static Position parse(String line) {
+		String[] values = line.split("\\,");
+		if (values.length == 3) {
+			Long timestamp = toLong(values[0]);
+			Double x = toDouble(values[1]);
+			Double y = toDouble(values[2]);
+			if (isValidTimestamp(timestamp) && isValidPosition(x, y)) {
+				return new Position(timestamp, x, y);
+			}
+		}
+		return null;
+	}
+
+	private static boolean isValidTimestamp(Long timestamp) {
+		return timestamp != null && timestamp >= 0;
+	}
+
+	private static boolean isValidPosition(Double x, Double y) {
+		// TODO test x/y > 1.0?
+		return x != null && y != null && x >= 0.0 && y >= 0.0;
+	}
+
+	private static Double toDouble(String val) {
+		try {
+			return Double.valueOf(val);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+	private static Long toLong(String val) {
+		try {
+			return Long.valueOf(val);
+		} catch (NumberFormatException e) {
+			return null;
 		}
 	}
 
