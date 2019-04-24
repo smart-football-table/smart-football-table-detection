aed45fef8cfa79b4529810582d9a3fb33431a9b2
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-04-25 00:36:57 +0200

    check if t2 is before t1

--- java-code/src/main/java/detection2/data/Movement.java
+++ java-code/src/main/java/detection2/data/Movement.java
@@ -21,6 +21,9 @@ public class Movement {
 	public Movement(Position pos1, Position pos2) {
 		this.distance = new Distance(sqrt(pow2(absDiffX(pos1, pos2)) + pow2(absDiffY(pos1, pos2))), CENTIMETER);
 		this.durationInMillis = pos2.getTimestamp() - pos1.getTimestamp();
+		if (this.durationInMillis < 0) {
+			throw new IllegalStateException("timestamp of pos2 (" + pos2 + ") before pos1 (" + pos1 + ")");
+		}
 		this.velocity = new Velocity(distance, this.durationInMillis, MILLISECONDS);
 	}
 
