ab204d81b28b6abda1ebfee3912a85981097a8ae
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-05-08 22:05:00 +0200

    purged src/test/resources

--- java-code/src/test/java/detection2/main/MainTest.java
+++ java-code/src/test/java/detection2/main/MainTest.java
@@ -1,12 +1,15 @@
 package detection2.main;
 
+import static java.nio.file.Files.write;
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -14,9 +17,26 @@ public class MainTest {
 
 	@Test
 	public void test() throws IOException {
-		String module = "src/test/resources/helloworld.py";
-		Stream<String> stream = Main.process(module, "-foo", "bar");
-		assertThat(stream.collect(toList()), is(asList("('Hello, world! ', \"['" + module + "', '-foo', 'bar']\")")));
+		String[] py = new String[] { //
+				"import sys", //
+				"print('Hello, world! ', str(sys.argv))" //
+		};
+
+		File tmpFile = fileWithContent(py);
+		try {
+			String module = tmpFile.getAbsolutePath();
+			assertThat(Main.process(module, "-foo", "bar").collect(toList()),
+					is(asList("('Hello, world! ', \"['" + module + "', '-foo', 'bar']\")")));
+		} finally {
+			tmpFile.delete();
+		}
+	}
+
+	private File fileWithContent(String[] content) throws IOException {
+		File file = File.createTempFile("test", ".py");
+		file.deleteOnExit();
+		write(file.toPath(), stream(content).collect(joining("\n")).getBytes());
+		return file;
 	}
 
 }
--- java-code/src/test/resources/helloworld.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-print('Hello, world! ', str(sys.argv))
-
